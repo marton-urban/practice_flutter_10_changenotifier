8e5870f9028b46c335891124dc402cd93ac3a490
Marton Urban <urban.marton@gmail.com>
2024-03-30 02:33:54 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'model/core_state.dart';
+import 'page/color_page.dart';
+import 'page/counter_page.dart';
+import 'widget/button_widget.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String title = 'ChangeNotifier';
+
+  @override
+  Widget build(BuildContext context) {
+    final state = CoreState();
+    return ListenableBuilder(
+        listenable: state,
+        builder: (BuildContext context, Widget? child) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: title,
+              theme: ThemeData(
+                scaffoldBackgroundColor: state.backgroundColor,
+                primaryColor: Colors.white,
+              ),
+              home: MainPage(state),
+            ));
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage(
+    this.state, {
+    super.key,
+  });
+
+  final CoreState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(MyApp.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ListenableBuilder(
+                listenable: state,
+                builder: (BuildContext context, Widget? child) => Text(
+                      '${state.counter}',
+                      style: const TextStyle(fontSize: 100),
+                    )),
+            const SizedBox(height: 47),
+            ButtonWidget(
+              text: 'Change Color',
+              onClicked: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => ColorPage(state),
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            ButtonWidget(
+              text: 'Change Counter',
+              onClicked: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => CounterPage(state),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/page/counter_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import '/model/core_state.dart';
+import '/widget/button_widget.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage(this.state, {super.key});
+
+  final CoreState state;
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 48),
+              child: buildTextField(onSubmitted: setStringCounter),
+            ),
+            const SizedBox(height: 24),
+            ButtonWidget(
+              text: 'Update Counter',
+              onClicked: () => setStringCounter(controller.text),
+            ),
+            const SizedBox(height: 64),
+            ButtonWidget(
+              text: 'Increment Counter',
+              onClicked: increment,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField({
+    required ValueChanged<String> onSubmitted,
+  }) =>
+      TextField(
+        controller: controller,
+        style: const TextStyle(color: Colors.white),
+        cursorColor: Colors.white,
+        keyboardType: TextInputType.number,
+        onSubmitted: onSubmitted,
+        decoration: InputDecoration(
+          hintText: 'Counter',
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(color: Colors.white),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(color: Colors.white),
+          ),
+          hintStyle: const TextStyle(color: Colors.white70),
+        ),
+      );
+
+  void increment() {
+    widget.state.incrementCounter();
+
+    Navigator.pop(context);
+  }
+
+  void setStringCounter(String value) {
+    final counter = int.tryParse(value);
+    widget.state.setCounter(counter!);
+
+    Navigator.pop(context);
+  }
+}
